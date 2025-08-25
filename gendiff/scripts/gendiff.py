588cc38021a757ed96ed0bc3a343dde7e83ff817
sirnapster88 <sir.napster@yandex.ru>
2025-08-25 11:44:12 +0300

    fix file errors and loading errors, adding generate_diff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,16 +2,35 @@ import argparse, json
 from pathlib import Path
 
 
-def load_files(file1_name, file2_name):
-    project_root = Path(__file__).parent.parent
-    file1_path = project_root / file1_name
-    file2_path = project_root / file2_name
+def load_files(file1_path, file2_path):
+    """Функция осуществляющая чтение и парсинг файлов"""
+    file1_path = 'file1.json'
+    file2_path = 'file2.json'
     with open(file1_path) as f1:
         data1 = json.load(f1)
     with open(file2_path) as f2:
         data2 = json.load(f2)
     return data1, data2
 
+def generate_diff(dict1, dict2):
+    """Функция осуществляющая поиск различий в двух плоских файлах"""
+    all_keys = sorted(set(dict1.keys()) | set(dict2.keys()))
+    result = []
+
+    for key in all_keys:
+        value1 = dict1.get(key)
+        value2 = dict2.get(key)
+        if key not in dict2:
+            result.append(f"    - {key}: {value1}")
+        elif key not in dict2:
+            result.append(f"    + {key}: {value2}")
+        elif value1 != value2:
+            result.append(f"    - {key}: {value1}")
+            result.append(f"    + {key}: {value2}")
+        else:
+            result.append(f"      {key}: {value1}")
+
+    return "{\n" + "\n".join(result) + "\n}"
 
 def main():
     parser = argparse.ArgumentParser(
@@ -26,12 +45,16 @@ def main():
     print(f"Comparing {args.first_file} with {args.second_file}")
     if args.format:
         print(f"Output format{args.format}")
+
     file1_data, file2_data= load_files(args.first_file, args.second_file)
     print(f"{args.first_file}:")
     print(json.dumps(file1_data))
     print(f"{args.second_file}:")
     print(json.dumps(file2_data))
-    
+
+    diff = generate_diff(file1_data, file2_data)
+    print('Difference:')
+    print(diff)
     
 
 if __name__ == '__main__':
