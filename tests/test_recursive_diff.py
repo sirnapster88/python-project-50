62505cf9dc54f82811341ed61bf0d1b629edcd60
sirnapster88 <sir.napster@yandex.ru>
2025-09-08 15:12:55 +0300

    update filestructure

--- tests/test_recursive_diff.py
+++ tests/test_recursive_diff.py
@@ -1,9 +1,13 @@
+import tempfile
+import pytest
+import shutil
+import json
+import os
 from gendiff.gendiff import generate_diff
 
 
-def test_recursive_diff():
-
-    result = generate_diff('file1.json', 'file2.json')
+def test_recursive_dif():
+    result = generate_diff('tests/file1.json', 'tests/file2.json')
 
     assert "common: {" in result
     assert "group1: {" in result
