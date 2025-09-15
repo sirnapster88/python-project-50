10f97fb18532fe8e8abdab90d44334fe3b4f398e
sirnapster88 <sir.napster@yandex.ru>
2025-09-15 14:49:25 +0300

    fix linter

--- tests/test_format_json.py
+++ tests/test_format_json.py
@@ -1,11 +1,11 @@
 import pytest
 from gendiff.scripts.gendiff import generate_diff
 
+
 @pytest.mark.parametrize("file1,file2,expected",
                          [('tests/test_data/input/test_file1.json',
                            'tests/test_data/input/test_file2.json',
                            'tests/test_data/expected/expected_json.txt')])
-
 def test_format_json(file1, file2, expected):
     result = generate_diff(file1, file2, 'json')
     with open(expected) as f:
--- tests/test_format_plain.py
+++ tests/test_format_plain.py
@@ -9,11 +9,11 @@ def test_format_value_plain():
     assert format_value_plain(True) == "true"
     assert format_value_plain({'a': 1}) == "[complex value]"
 
+
 @pytest.mark.parametrize("file1,file2, expected",
                          [('tests/test_data/input/test_file1.json',
                            'tests/test_data/input/test_file2.json',
                            'tests/test_data/expected/expected_plain.txt')])
-
 def test_format_plain(file1, file2, expected):
     result = generate_diff(file1, file2, 'plain')
     with open(expected) as f:
