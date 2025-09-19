7a4e719b86bf917c124ad50eec67184043d34621
sirnapster88 <sir.napster@yandex.ru>
2025-09-19 22:03:56 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -15,45 +15,47 @@ EMPTY = INPUT / 'empty'
 
 @pytest.mark.parametrize("file1,file2,format_name,expected",
                          # тестируем формат по умолчанию(stylish)
-                         [(EQUAL / 'file1.json',
+                         [
+                          [EQUAL / 'file1.json',
                            EQUAL / 'file2.json',
                            'stylish',
-                           EXPECTED / 'expected_equal_files.txt'),
-                           (DIFF_VALUES / 'file1.json',
+                           EXPECTED / 'expected_equal_files.txt'],
+                           [DIFF_VALUES / 'file1.json',
                             DIFF_VALUES / 'file2.json',
                             'stylish',
-                            EXPECTED / 'expected_diff_values.txt'),
-                           (DIFF_KEYS / 'file1.json',
+                            EXPECTED / 'expected_diff_values.txt'],
+                           [DIFF_KEYS / 'file1.json',
                             DIFF_KEYS / 'file2.json',
                             'stylish',
-                            EXPECTED / 'expected_diff_keys.txt'),
-                           (EMPTY_NOEMPTY / 'file1.json',
+                            EXPECTED / 'expected_diff_keys.txt'],
+                           [EMPTY_NOEMPTY / 'file1.json',
                             EMPTY_NOEMPTY / 'file2.json',
                             'stylish',
-                            EXPECTED / 'exp_empty_noempty.txt'),
-                           (EMPTY / 'file1.json',
+                            EXPECTED / 'exp_empty_noempty.txt'],
+                           [EMPTY / 'file1.json',
                             INPUT / 'empty/file1.json',
                             'stylish',
-                            EXPECTED / 'expected_empty.txt'),
-                           (TEST_DATA / 'file1_tree.json',
+                            EXPECTED / 'expected_empty.txt'],
+                           [TEST_DATA / 'file1_tree.json',
                             TEST_DATA / 'file2_tree.json',
                             'stylish',
-                            EXPECTED / 'expected_recursive.txt'),
+                            EXPECTED / 'expected_recursive.txt'],
                             # тестируем json-форматтер
-                            (TEST_DATA / 'file1_tree.json',
+                            [TEST_DATA / 'file1_tree.json',
                              TEST_DATA / 'file2_tree.json',
                              'json',
-                            EXPECTED / 'expected_json.txt'),
+                            EXPECTED / 'expected_json.txt'],
                             # тестируем yaml-форматтер
-                            (TEST_DATA / 'file1.yml',
+                            [TEST_DATA / 'file1.yml',
                              TEST_DATA / 'file2.yml',
                             'stylish',
-                            EXPECTED / 'expected_yaml.txt'),
+                            EXPECTED / 'expected_yaml.txt'],
                             # тестируем plain-форматтер
-                            (TEST_DATA / 'file1_tree.json',
+                            [TEST_DATA / 'file1_tree.json',
                              TEST_DATA / 'file2_tree.json',
                              'plain',
-                             EXPECTED / 'expected_plain.txt')])
+                             EXPECTED / 'expected_plain.txt']
+                             ])
 def test_gendiff(file1, file2, format_name, expected):
     result = generate_diff(file1, file2, format_name)
     with open(expected) as f:
