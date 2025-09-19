802f45624a8aa8714c6b63c06904930de6156244
sirnapster88 <sir.napster@yandex.ru>
2025-09-19 21:26:56 +0300

    update tests'

--- tests/test_yaml_files.py
+++
@@ -1,9 +0,0 @@
-from gendiff.find_diff import generate_diff
-
-
-def test_yaml_files():
-    result = generate_diff('tests/test_data/file1.yml',
-                           'tests/test_data/file2.yml')
-    with open('tests/test_data/expected/expected_yaml.txt') as f:
-        expected = f.read()
-        assert result == expected
